89e6cf2ae5a78b03ca97d7b5d18fcbc89c4860df
María Fernanda Garay Núñez <Ferna@DESKTOP-QLEQQ75>
2022-09-15 17:16:03 -0300

    se agrega clase de suscripcion

---
+++ PGY2121Prueba_1/src/cl/fernandagarayn/dto/Suscripcion.java
@@ -0,0 +1,72 @@
+package cl.fernandagarayn.dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Ferna
+ */
+public class Suscripcion {
+    private int numero;
+    private Usuario usuario;
+    private Date fechaInicio;
+    private int abonoTotal;
+    private String equipos;
+    private int correlativoVenta;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getAbonoTotal() {
+        return abonoTotal;
+    }
+
+    public void setAbonoTotal(int abonoTotal) {
+        this.abonoTotal = abonoTotal;
+    }
+
+    public String getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(String equipos) {
+        this.equipos = equipos;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public int getCorrelativoVenta() {
+        return correlativoVenta;
+    }
+
+    public void setCorrelativoVenta(int correlativoVenta) {
+        this.correlativoVenta = correlativoVenta;
+    }
+    
+    public void agregarClubDeportivo(ClubDeportivo club){
+        if(!this.equipos.equals("")){
+            this.equipos += "; ";
+        }
+        this.equipos += club.toString();
+        this.abonoTotal += club.getValorSuscripcion();
+    }
+}
