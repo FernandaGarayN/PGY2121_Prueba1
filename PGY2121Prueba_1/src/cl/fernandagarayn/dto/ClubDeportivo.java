6114bbd372a3cf04c7c88f2ffc571dd7be6f2669
María Fernanda Garay Núñez <Ferna@DESKTOP-QLEQQ75>
2022-09-15 17:15:40 -0300

    se agrega clase de club deportivo

---
+++ PGY2121Prueba_1/src/cl/fernandagarayn/dto/ClubDeportivo.java
@@ -0,0 +1,85 @@
+package cl.fernandagarayn.dto;
+
+/**
+ *
+ * @author Ferna
+ */
+public class ClubDeportivo {
+    private String codigo;
+    private String nombre;
+    private String nombreFundador;
+    private int anioFundacion;
+    private String paisOrigen;
+    private String lema;
+    private int valorSuscripcion;
+    private String colores;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLema() {
+        return lema;
+    }
+
+    public void setLema(String lema) {
+        this.lema = lema;
+    }
+
+    public int getValorSuscripcion() {
+        return valorSuscripcion;
+    }
+
+    public void setValorSuscripcion(int valorSuscripcion) {
+        this.valorSuscripcion = valorSuscripcion;
+    }
+
+    public String getColores() {
+        return colores;
+    }
+
+    public void setColores(String colores) {
+        this.colores = colores;
+    }
+
+    public String getNombreFundador() {
+        return nombreFundador;
+    }
+
+    public void setNombreFundador(String nombreFundador) {
+        this.nombreFundador = nombreFundador;
+    }
+
+    public int getAnioFundacion() {
+        return anioFundacion;
+    }
+
+    public void setAnioFundacion(int anioFundacion) {
+        this.anioFundacion = anioFundacion;
+    }
+
+    public String getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public void setPaisOrigen(String paisOrigen) {
+        this.paisOrigen = paisOrigen;
+    }
+
+    @Override
+    public String toString() {
+        return codigo + " $" + valorSuscripcion;
+    }
+}
